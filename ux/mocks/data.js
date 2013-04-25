680539704ffb7afa2c9839ec8b35886b5acde296
Jake Solomon <jacob@codeforamerica.org>
2013-04-24 17:12:15 -0700

    basic flows and reminder variants

--- ux/mocks/data.js
+++ ux/mocks/data.js
@@ -1,52 +1,75 @@
 var MOCKS = {
-  title: 'Hermes UX',
+  title: 'Landshark',
 
   categories: [
     {
-      title: 'v1',
+        title: 'Basic flow',
 
-      mocks: [
-        { subcategory: 'Text messages: Poster input/opt-in' },
-        { title: 'Basic flow', url: 'mocks/sites/v1-poster.html' },
-        { title: 'Variants', url: 'mocks/sites/v1-judge-variants.html' },
-        { title: 'Errors and corner cases', url: 'mocks/sites/v1-poster-errors.html' },
-        { subcategory: 'Text messages: Judge input' },
-        { title: 'Basic flow', url: 'mocks/sites/v1-judge.html' },
-        { title: 'Errors and corner cases', url: 'mocks/sites/v1-judge-errors.html' },
-        { subcategory: 'Posters for opt-in' },
-        { title: 'Mini cards', url: 'mocks/physical/cards-mini.png' },
-        { title: 'Business cards', url: 'mocks/physical/cards-business.png' },
-        { title: 'Hybrid cards', url: 'mocks/physical/cards-clerk.png' },
-        { title: 'Plasma display at the HoJ', url: 'mocks/physical/plasma.jpg' },
-        { subcategory: 'Control panel' },
-        { title: 'Homepage', url: 'mocks/images/03/cp-homepage.png' },
-        { title: 'Details (judge flow)', url: 'mocks/images/03/cp-judge-good.png' },
-        { title: 'Details (judge – failure to deliver)', url: 'mocks/images/03/cp-judge-fail-to-send.png' },
-        { title: 'Details (poster flow)', url: 'mocks/images/03/cp-poster-good.png' },
-        { title: 'Details (poster – failure to verify)', url: 'mocks/images/03/cp-poster-fail-to-verify.png' },        
-      ]
+        mocks: [
+        { title: 'Reminder', url: 'mocks/sites/basic.html' },
+        { title: 'Stop', url: 'mocks/sites/stop.html' },
+        { title: 'Other', url: 'mocks/sites/other.html' },
+        // { title: 'Call center', url: 'mocks/sites/v2-poster.html' },
+        // { title: 'In person', url: 'mocks/sites/v3-poster.html' },
+        ]
     },
-    {
-      title: 'Future explorations',
 
-      mocks: [
-        { subcategory: 'Text messages' },
-        { title: 'Basic flow', url: 'mocks/sites/basic-flow.html' },
-        { title: 'Interaction', url: 'mocks/sites/interaction.html' },
-        { title: 'Language variants', url: 'mocks/sites/language-variants.html' },
-        { title: 'Hint variants', url: 'mocks/sites/hint-variants.html' },
-        { title: 'Court date change', url: 'mocks/sites/court-date-changed.html' },
-        { title: 'Friends and family', url: 'mocks/sites/friends-and-family.html' },
-        { title: 'Remote opt-in', url: 'mocks/sites/remote-opt-in.html' },
-        { subcategory: 'Control panel' },
-        { title: 'Homepage (All time)', url: 'mocks/images/01/dashboard-homepage-all-time.png' },
-        { subcategory: 'Site' },
-        { title: 'Two weeks before', url: 'mocks/images/01/site-homepage-two-weeks.png' },
-        { title: 'One day before', url: 'mocks/images/01/site-homepage-day-before.png' },
-        { title: 'One day after', url: 'mocks/images/01/site-homepage-day-after.png' },
-        { title: 'How do I get there?', url: 'mocks/images/01/site-how-do-i-get-there.png' },
-        { title: 'What if I don’t show up?', url: 'mocks/images/01/site-what-if-i-dont-show-up.png' }
-      ]
-    }
+    {
+        title: 'Variants',
+
+        mocks: [
+        { title: 'Reminder variants', url: 'mocks/sites/reminder-variants.html' },
+        ]
+    },
+
+    // {
+    //   title: 'Basic flow',
+
+    //   mocks: [
+    //     { subcategory: 'Text messages: Poster input/opt-in' },
+    //     { title: '1. full info', url: 'mocks/sites/v1-poster.html' },
+    //     { title: '2. no name', url: '' },
+    //     { title: '3. no name, no date', url: '' },
+    //     { title: '4. general', url: '' },
+    //     { title: 'Variants', url: 'mocks/sites/v1-judge-variants.html' },
+    //     { title: 'Errors and corner cases', url: 'mocks/sites/v1-poster-errors.html' },
+    //     { subcategory: 'Text messages: Judge input' },
+    //     { title: 'Basic flow', url: 'mocks/sites/v1-judge.html' },
+    //     { title: 'Errors and corner cases', url: 'mocks/sites/v1-judge-errors.html' },
+    //     { subcategory: 'Posters for opt-in' },
+    //     { title: 'Mini cards', url: 'mocks/physical/cards-mini.png' },
+    //     { title: 'Business cards', url: 'mocks/physical/cards-business.png' },
+    //     { title: 'Hybrid cards', url: 'mocks/physical/cards-clerk.png' },
+    //     { title: 'Plasma display at the HoJ', url: 'mocks/physical/plasma.jpg' },
+    //     { subcategory: 'Control panel' },
+    //     { title: 'Homepage', url: 'mocks/images/03/cp-homepage.png' },
+    //     { title: 'Details (judge flow)', url: 'mocks/images/03/cp-judge-good.png' },
+    //     { title: 'Details (judge – failure to deliver)', url: 'mocks/images/03/cp-judge-fail-to-send.png' },
+    //     { title: 'Details (poster flow)', url: 'mocks/images/03/cp-poster-good.png' },
+    //     { title: 'Details (poster – failure to verify)', url: 'mocks/images/03/cp-poster-fail-to-verify.png' },        
+    //   ]
+    // },
+    // {
+    //   title: 'Future explorations',
+
+    //   mocks: [
+    //     { subcategory: 'Text messages' },
+    //     { title: 'Basic flow', url: 'mocks/sites/basic-flow.html' },
+    //     { title: 'Interaction', url: 'mocks/sites/interaction.html' },
+    //     { title: 'Language variants', url: 'mocks/sites/language-variants.html' },
+    //     { title: 'Hint variants', url: 'mocks/sites/hint-variants.html' },
+    //     { title: 'Court date change', url: 'mocks/sites/court-date-changed.html' },
+    //     { title: 'Friends and family', url: 'mocks/sites/friends-and-family.html' },
+    //     { title: 'Remote opt-in', url: 'mocks/sites/remote-opt-in.html' },
+    //     { subcategory: 'Control panel' },
+    //     { title: 'Homepage (All time)', url: 'mocks/images/01/dashboard-homepage-all-time.png' },
+    //     { subcategory: 'Site' },
+    //     { title: 'Two weeks before', url: 'mocks/images/01/site-homepage-two-weeks.png' },
+    //     { title: 'One day before', url: 'mocks/images/01/site-homepage-day-before.png' },
+    //     { title: 'One day after', url: 'mocks/images/01/site-homepage-day-after.png' },
+    //     { title: 'How do I get there?', url: 'mocks/images/01/site-how-do-i-get-there.png' },
+    //     { title: 'What if I don’t show up?', url: 'mocks/images/01/site-what-if-i-dont-show-up.png' }
+    //   ]
+    // }
   ]
 };
